029b8232f88b014342cd1a9560124900c92aa1b5
Daniel36191 <dmoeller3998@gmail.com>
2026-04-23 16:29:58 -0400

    audio-man desktop icon

--- packages/audio-man.nix
+++ packages/audio-man.nix
@@ -19,8 +19,8 @@ stdenv.mkDerivation (finalAttrs: {
   src = fetchFromGitHub {
     owner = "Daniel36191";
     repo = "audio-man";
-    rev = "e41730ecf1480500517f3c3a657c6348c610594c";
-    hash = "sha256-0zkfh4cPolNFE8sDoZt9s0YdwMQHqS5wmUVBW1YbDaU=";
+    rev = "7aff087afd4c213d423b9d4bdf96807f53e2e717";
+    hash = "sha256-4nJ+SUHY4PiQsMG+vHulbnKrZqkwPCe9VDBa7zbGUVM=";
   };
 
   nativeBuildInputs = [
@@ -41,8 +41,11 @@ stdenv.mkDerivation (finalAttrs: {
     runHook preInstall
 
     mkdir -p $out/bin
-    mkdir -p $out/share
+    mkdir -p $out/share/applications
+    mkdir -p $out/share/icons/hicolor/128x128/apps
     cp -r * $out/share
+    install -m644 $src/audio-man.desktop $out/share/applications/audio-man.desktop
+    install -m644 $src/audio-man.png $out/share/icons/hicolor/128x128/apps/audio-man.png
     ags bundle app.ts $out/bin/audio-man -d "SRC='$out/share'"
 
     runHook postInstall
